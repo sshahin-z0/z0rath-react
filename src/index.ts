a86dcd9c46315d38d8435331062a92155c43940e
Shadi Shaheen <shadizr4@gmail.com>
2025-01-04 23:35:19 +0400

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {default as HasPermission} from "./components/HasPermission";
-export {default as Z0AuthzProvider} from "./components/Z0rathProvider"
+export {default as Z0rathProvider} from "./components/Z0rathProvider"
 export {useZ0rath} from "./components/Z0rathContext"
